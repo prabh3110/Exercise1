55543d9f1efbd1ecb46624270417f24df6efa8a2
prabh3110 <122491346+prabh3110@users.noreply.github.com>
2023-02-08 16:11:09 -0500

    Update CardTrick.java
    Added my information

--- src/exercise1/CardTrick.java
+++ src/exercise1/CardTrick.java
@@ -53,19 +53,19 @@ public class CardTrick {
         System.out.println("Congratulations, you guessed right!");
         System.out.println();
         
-        System.out.println("My name is Paul, but you can call me prof, Paul or sir");
+        System.out.println("My name is Prabal, but you can call me prabh");
         System.out.println();
         
-        System.out.println("My career ambitions:");
+        System.out.println("My career ambitions:Software programer");
         System.out.println("-- Be more active on LinkedIn");
         System.out.println("-- Have a semester with no violations of academic integrity!");
 	System.out.println();	
 
         System.out.println("My hobbies:");
         System.out.println("-- Investing");
-        System.out.println("-- Cooking");
-        System.out.println("-- Reading/Watching TV");
-        System.out.println("-- Riding my motorcycle");
+        System.out.println("-- Watching anime");
+        System.out.println("-- Programming");
+        System.out.println("-- Games");
 
         System.out.println();
         
