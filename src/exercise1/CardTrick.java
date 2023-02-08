7bf1fcb6f2ef6bbaee5834dddc5d7ff53029d2f1
Prabal Manchanda <manchapr@sheridancollege.ca>
2023-02-08 16:18:44 -0500

    I am done

--- src/exercise1/CardTrick.java
+++ src/exercise1/CardTrick.java
@@ -50,6 +50,7 @@ public class CardTrick {
      */
     private static void printInfo() {
     
+        System.out.println("Im done!");
         System.out.println("Congratulations, you guessed right!");
         System.out.println();
         
